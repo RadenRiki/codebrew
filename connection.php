3eaea17b46c2eed26d7fd3ac988539dadcb682bc
collegeNaylannajwa <23523183@students.uii.ac.id>
2025-06-17 21:16:53 +0700

    fix connection and folder name

--- connection.php
+++ connection.php
@@ -1,11 +1,12 @@
 <?php
-$host = "Localhost";
+$host = "localhost";
 $user = "root";
 $password = "";
 $database = "codebrew_db";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
-if (!$conn -> connect_error) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+?>
