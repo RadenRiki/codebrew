2c9b4efbb7ee0080349cc56cc3ce52992c7a85c5
Raden Riki <rkiraden@gmail.com>
2025-06-21 08:32:53 +0700

    a

--- homepage/config.php
+++ homepage/config.php
@@ -1,11 +1,1 @@
-<?php
-// Konfigurasi database
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root'); // Ganti dengan username MySQL Anda
-define('DB_PASSWORD', 'root'); // Ganti dengan password MySQL Anda
-define('DB_NAME', 'codebrew_db'); // Ganti dengan nama database Anda
-
-// API key Groq
-define('GROQ_API_KEY', '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');
-define('GROQ_ENDPOINT', 'https://api.groq.com/openai/v1/chat/completions');
-?>
+ahka
