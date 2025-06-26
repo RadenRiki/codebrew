4ad31d8bc1857f0298d2bd7eeef784a61d6bb3ad
Ikhsan A <ikhsanarifianto74@gmail.com>
2025-06-26 23:08:35 +0700

    ranking:)

--- homepage/ranking.php
+++ homepage/ranking.php
@@ -27,6 +27,9 @@ mysqli_close($conn);
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
   <link rel="stylesheet" href="../landing-page/index.css" />
+  <link rel="stylesheet" href="../ranking2.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"/>
+
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <style>
     .user-profile-container {
